0eb28111e63296ea7528664b62954bc897a1dec6
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-12-10 13:28:59 +0100

    Updates: 1-hbnb.py

---
+++ web_dynamic/1-hbnb.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" 1-hbnb.py solution """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template, request
+import uuid
+
+app = Flask(__name__)
+
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+
+    # Optimize sorting
+    states = sorted(storage.all(State).values(), key=lambda k: k.name)
+    cities = {}  # Create dictionary to avoid unnecessary sorting
+    for state in states:
+        cities[state.id] = sorted(state.cities, key=lambda k: k.name)
+
+    amenities = sorted(storage.all(Amenity).values(), key=lambda k: k.name)
+
+    # Optimize data transfer and lazy loading
+    places = {}  # Dictionary for lazy loading places
+    def get_places(state_id):
+        if state_id not in places:
+            places[state_id] = storage.all(Place).filter(Place.state_id == state_id)
+        return places[state_id]
+
+    cache_id = uuid.uuid4()
+
+    return render_template('0-hbnb.html',
+                           states=states,
+                           cities=cities,
+                           amenities=amenities,
+                           get_places=get_places,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
