c9f6000c33418ced485cb18c58d6ffb06bc217a9
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-12-10 13:15:43 +0100

    updates: 0-hbnb.py

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" 0-hbnb.py solution"""
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template, request
+import uuid
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """ start  """
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    cache_id = uuid.uuid4()
+
+    """Optimized sorting"""
+    states = sorted(states, key=lambda k: k.name)
+    cities = {}  """ Dictionary to store cities with state association """
+    for state in states:
+        state_cities = sorted(state.cities, key=lambda k: k.name)
+        cities[state.id] = state_cities
+
+    """ Lazy loading of places """
+    places = {}  """ Dictionary to store places with lazy loading """
+    def get_places(state_id):
+        if state_id not in places:
+            places[state_id] = storage.all(Place).filter(Place.state_id == state_id)
+        return places[state_id]
+
+    return render_template('0-hbnb.html',
+                           states=states,
+                           cities=cities,
+                           amenities=amenities,
+                           get_places=get_places,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
