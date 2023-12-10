4386878f7391d2280993cd7777c3f5d45121e5dc
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-12-10 13:29:32 +0100

    Updates: 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -10,7 +10,8 @@ from flask import Flask, render_template, request
 import uuid
 
 app = Flask(__name__)
-
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 @app.teardown_appcontext
 def close_db(error):
